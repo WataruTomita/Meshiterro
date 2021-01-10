ba6d3321a88f2754549604ff98b113456b565752
WataruTomita <tomi.symphonia@gmail.com>
2021-01-10 04:52:33 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e7dc63917348687191898f978e575b53eca4d18c4f384f169b208156e6dcfa4f96a2582cde430c6e8a619a5ef7de8c1169aa997afd07475e1c1f7a5168f5435f'
